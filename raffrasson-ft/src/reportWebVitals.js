58a2d4caf0fa1eb7b46d19ac69144794a417b097
raffrasson <raf.frasson@gmail.com>
2022-03-29 06:34:36 -0300

    remove erros de lint

--- raffrasson-ft/src/reportWebVitals.js
+++ raffrasson-ft/src/reportWebVitals.js
@@ -1,13 +1,15 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
 
-export default reportWebVitals;
+// export default reportWebVitals;
+
+// com base em resposta do slack removi as exportações e importações: https://trybecourse.slack.com/archives/C027T2VU8U8/p1633642576260800?thread_ts=1633642437.259600&cid=C027T2VU8U8
